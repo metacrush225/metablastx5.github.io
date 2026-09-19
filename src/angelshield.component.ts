e8ee8be8402a456a84b590d9b39de49d7e3b5373
metacrush225 <metacrush225@gmail.com>
2026-09-19 08:09:56 +0200

    Installation de prettier

--- src/angelshield.component.ts
+++ src/angelshield.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-@Component({selector:'app-angelshield',standalone:true,imports:[RouterLink],templateUrl:'./angelshield.component.html',host:{class:'product-page'}})
-export class AngelShieldComponent { readonly play='https://play.google.com/store/apps/details?id=com.kurt.angelshieldplus'; }
+import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
+@Component({
+  selector: "app-angelshield",
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: "./angelshield.component.html",
+  host: { class: "product-page" },
+})
+export class AngelShieldComponent {
+  readonly play =
+    "https://play.google.com/store/apps/details?id=com.kurt.angelshieldplus";
+}
